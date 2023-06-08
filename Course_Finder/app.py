d4c6d37ade7af22eb59a561e85621b89e05c20c5
ulriku <ulrikhlp@gmail.com>
2023-06-08 20:23:09 +0200

    started working on filtering courses

--- Course_Finder/app.py
+++ Course_Finder/app.py
@@ -28,13 +28,13 @@ def get_db_conn():
         port=port,
     )
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
     conn = get_db_conn()
     curs = conn.cursor()
 
     # Join tables to fetch course data
-    curs.execute("""
+    query = """
         SELECT c.name, b.number AS block, c.duration, array_agg(pr.name) AS prerequisites, p.name AS professor, cr.average_grade, et.name AS exam_type, c.ECTS
         FROM Course c
         LEFT JOIN CoursePrerequisite cp ON cp.course_id = c.id
@@ -47,10 +47,39 @@ def home():
         LEFT JOIN CourseBlock cb ON cb.course_id = c.id
         LEFT JOIN Block b ON b.number = cb.block_number
         GROUP BY c.id, b.number, p.name, cr.average_grade, et.name
-    """)
+    """
 
+    if request.method == 'POST':
+        # Handle filtering form submission
+        # Retrieve filtering criteria from the request object
+        # Construct the SQL query with the filtering conditions
+        # Execute the query and fetch the filtered data
+        # Assign the filtered data to a variable for rendering
+        selected_block = request.form.get('block')
+        selected_professor = request.form.get('professor')
+        selected_exam_type = request.form.get('exam_type')
+        selected_grade = request.form.get('grade')
+        selected_ects = request.form.get('ects')
+        selected_duration = request.form.get('duration')
+        selected_prerequisite = request.form.get('prerequisite')
+
+        if selected_block:
+            query += f" AND b.number = {selected_block}"
+        if selected_professor:
+            query += f" AND p.name = '{selected_professor}'"
+        if selected_exam_type:
+            query += f" AND et.name = '{selected_exam_type}'"
+        if selected_grade:
+            query += f" AND cr.average_grade >= {selected_grade}"
+        if selected_ects:
+            query += f" AND c.ECTS = {selected_ects}"
+        if selected_duration:
+            query += f" AND c.duration = {selected_duration}"
+        if selected_prerequisite:
+            query += f" AND pr.name = '{selected_prerequisite}'"
+
+    curs.execute(query)
     data = curs.fetchall()
-
     conn.close()
 
     return render_template('home.html', data=data)
---
+++ Course_Finder/drop_schema.sql
@@ -0,0 +1,14 @@
+-- Drop tables in reverse order of creation to avoid foreign key constraint errors
+DROP TABLE IF EXISTS StudentReview;
+DROP TABLE IF EXISTS CourseBlock;
+DROP TABLE IF EXISTS CourseExamType;
+DROP TABLE IF EXISTS Staff;
+DROP TABLE IF EXISTS CourseResult;
+DROP TABLE IF EXISTS ExamType;
+DROP TABLE IF EXISTS Block;
+DROP TABLE IF EXISTS Student;
+DROP TABLE IF EXISTS Review;
+DROP TABLE IF EXISTS CoursePrerequisite;
+DROP TABLE IF EXISTS Course;
+DROP TABLE IF EXISTS Role;
+DROP TABLE IF EXISTS Professor;
