2ddd6347db754e0233546a5a4d7999a03627c8c8
unknown <petertmikkelsen3@gmail.com>
2023-06-10 15:10:27 +0200

    Progress on completed courses

---
+++ Course_Finder/Routes/Completed_Courses.py
@@ -0,0 +1,38 @@
+from flask import render_template, flash, redirect, url_for
+from forms import CompletedCourseForm
+from models import save_completed_course, get_completed_courses, select_Course_by_name
+from flask_login import current_user
+
+def init_Completed_Courses(app, get_db_conn):
+
+    @app.route('/completed_courses', methods=['GET', 'POST'])
+    def completed_courses():
+        conn = get_db_conn()
+        cur = conn.cursor()
+
+        # Ensure user is logged in
+        if not current_user.is_authenticated:
+            flash('You need to be logged in to access this page.', 'danger')
+            return redirect(url_for('login'))
+
+        form = CompletedCourseForm()
+
+        # Handling form submission to register a completed course
+        if form.validate_on_submit():
+            course_id = form.course_id.data
+            completion_date = form.completion_date.data
+
+            # Insert into CompletedCourses table
+            save_completed_course(current_user.id, course_id, completion_date, conn)
+            flash('Course registered as completed!', 'success')
+
+        # Retrieve the completed courses for the current user
+        completed_courses = get_completed_courses(current_user.id, get_db_conn)
+
+        cur.execute("SELECT id, name FROM Course;")
+        courses = cur.fetchall()
+
+        cur.close()
+        conn.close()
+
+        return render_template('completed_courses.html', form=form, completed_courses=completed_courses, courses=courses)
--- Course_Finder/forms.py
+++ Course_Finder/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, EqualTo
+from wtforms import IntegerField, DateField
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -12,3 +13,8 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     name = StringField('Name', validators=[DataRequired()])
+
+
+class CompletedCourseForm(FlaskForm):
+    course_id = IntegerField('Course ID', validators=[DataRequired()])
+    completion_date = DateField('Completion Date', validators=[DataRequired()], format='%Y-%m-%d')
