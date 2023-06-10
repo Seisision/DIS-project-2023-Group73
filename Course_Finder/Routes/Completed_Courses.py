05badba7d685a76a1fdb9950ae7fa005010b8e23
ulriku <ulrikhlp@gmail.com>
2023-06-10 16:12:04 +0200

    written course reviews are now correctly inserted int othe relational table between student and review
    fixed display bug with completed courses

--- Course_Finder/Routes/Completed_Courses.py
+++ Course_Finder/Routes/Completed_Courses.py
@@ -19,15 +19,17 @@ def init_Completed_Courses(app, get_db_conn):
 
         # Handling form submission to register a completed course
         if form.validate_on_submit():
+            print('Form validated')
             course_id = form.course_id.data
             completion_date = form.completion_date.data
 
             # Insert into CompletedCourses table
             try:
-                save_completed_course(current_user.id, course_id, completion_date, conn)
+                save_completed_course(current_user.id, course_id, completion_date, get_db_conn)
                 flash('Course registered as completed!', 'success')
             except Exception as e:
                 flash(f'Error saving completed course: {str(e)}', 'danger')
+                print(f'Error saving completed course: {str(e)}')
 
         # Retrieve the completed courses for the current user
         completed_courses = get_completed_courses(current_user.id, get_db_conn)
@@ -39,4 +41,3 @@ def init_Completed_Courses(app, get_db_conn):
         conn.close()
 
         return render_template('completed_courses.html', form=form, completed_courses=completed_courses, courses=courses)
-
